0ba5f9d65d17a622d0ac1cb97d9f2cac2ff42294
wen1 <wen1@users.noreply.github.com>
2013-09-11 11:53:27 -0400

    Update BiggestSumSequence.java

--- BiggestSumSequence.java
+++ BiggestSumSequence.java
@@ -1,3 +1,23 @@
 class BiggestSumSequence {
-
+  public static int BiggestsSumSequence(int[] s){
+      // find the first one +num
+      int i;
+      for ( i = 0; i < s.length-1, i++){
+          if (s[i] >=0) break;
+      }
+      return BiggestSumSequence(i,s.length-1,s);
+  }
+  // return the sum of the biggest sequence 
+  private int BiggestSumSequence(int start, int end, int[] s){
+      if (end == 0){
+          return 0;
+      } else {
+          int temp =  sum(end+1,s.length-1, s);
+          if (  >=0 ){
+              return BiggestSumSequence(start, end-1, s) + 
+          } else {
+               
+          }
+      }
+  }
 }
